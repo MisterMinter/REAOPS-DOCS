9380d64f06217c06e4a89ed3fad1369aa8ca1faa
DeVAE0 <devae0@mac.lan>
2026-04-10 20:35:55 -0700

    fix: Remove invalid ProgressPlugin options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,27 @@ module.exports = {
   organizationName: 'MisterMinter',
   projectName: 'REAOPS-DOCS',
   onBrokenLinks: 'throw',
+  webpack: {
+    configure: (config) => {
+      // Remove invalid ProgressPlugin options that newer webpack doesn't support
+      const progressPlugins = config.plugins?.filter(p => 
+        p && p.constructor && p.constructor.name === 'ProgressPlugin'
+      );
+      
+      if (progressPlugins) {
+        progressPlugins.forEach(plugin => {
+          // The ProgressPlugin stores options in a _options property
+          if (plugin._options) {
+            delete plugin._options.name;
+            delete plugin._options.color;
+            delete plugin._options.reporters;
+            delete plugin._options.reporter;
+          }
+        });
+      }
+      return config;
+    },
+  },
   presets: [
     [
       'classic',
