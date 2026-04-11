e9a0895cf8d2d321f80d62dce30352dc967733ff
DeVAE0 <devae0@mac.lan>
2026-04-10 18:35:09 -0700

    fix: Minimal config without docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   title: "REAOPS Docs",
+  tagline: "Documentation",
   url: "https://misterminter.github.io",
   baseUrl: "/REAOPS-DOCS/",
   organizationName: "MisterMinter",
   projectName: "REAOPS-DOCS",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-  presets: ["@docusaurus/preset-classic"],
 };
