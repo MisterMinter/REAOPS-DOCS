6daf1b4eac57414fa13bc14c016a002fdccf7a62
DeVAE0 <devae0@mac.lan>
2026-04-10 20:30:55 -0700

    fix: Use Docusaurus 3.7.0 with Node 18

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,5 @@
+export default [
+  require("/private/tmp/REAOPS-DOCS/node_modules/infima/dist/css/default/default.css"),
+  require("/private/tmp/REAOPS-DOCS/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/private/tmp/REAOPS-DOCS/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+];
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,19 +6,6 @@ module.exports = {
   organizationName: 'MisterMinter',
   projectName: 'REAOPS-DOCS',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  webpack: {
-    configure: (webpackConfig, isServer) => {
-      // Filter out any plugin with ProgressPlugin or similar
-      if (webpackConfig.plugins) {
-        webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => {
-          const name = plugin.constructor?.name || '';
-          return !name.includes('Progress');
-        });
-      }
-      return webpackConfig;
-    },
-  },
   presets: [
     [
       'classic',
