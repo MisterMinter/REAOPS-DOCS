15a6e5ca3f9e99916402f509e29f0004e06c2234
DeVAE0 <devae0@mac.lan>
2026-04-10 18:28:38 -0700

    fix: Absolute minimum config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,21 +1,10 @@
 module.exports = {
   title: "REAOPS Docs",
-  tagline: "Documentation",
   url: "https://misterminter.github.io",
   baseUrl: "/REAOPS-DOCS/",
   organizationName: "MisterMinter",
   projectName: "REAOPS-DOCS",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          routeBasePath: "/",
-          sidebarPath: false,
-        },
-      },
-    ],
-  ],
+  presets: ["@docusaurus/preset-classic"],
 };
